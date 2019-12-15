02cd0ed64b862bc0d72b672fbfa3bc96a25b082b
BryanSer <bryan_lzh@njust.edu.cn>
2019-12-15 11:59:16 +0800

    init

---
+++ src/main/java/com/github/bryanser/twinking/Main.kt
@@ -0,0 +1,11 @@
+package com.github.bryanser.twinking
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onEnable() {
+    }
+
+    override fun onDisable() {
+    }
+}
